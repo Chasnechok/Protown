eae80612290674baaf19a863dd5e5c9361e1b3c5
Artem Sukhorukov <artem@sukhorukov.me>
2023-11-30 20:46:50 +0100

    add county.ua link to the footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,18 +1,27 @@
 <script>
-
+  const year = new Date().getFullYear()
 </script>
 
-<style>
-    .footer-content {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-wrap: wrap;
-        color: #7d7d7d;
-        height: 2em;
-    }
-</style>
+<div class="footer-content">
+  <a href="https://www.country.ua/" target="_blank" class="country-ua-link"
+    ><img src="https://www.country.ua/img/logo.svg" height="25" border="0" alt="country.ua" /></a
+  >
+  <span>© {year} Все права защищены.</span>
+</div>
 
-    <div class="footer-content">
-        <span>© 2021 Все права защищены.</span>
-    </div>
+<style>
+  .footer-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+    color: #7d7d7d;
+  }
+  .country-ua-link {
+    padding: 5px;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    border-radius: 3px;
+    display: flex;
+  }
+</style>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,48 +1,44 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-	import Footer from '../components/Footer.svelte';
-	//export let segment;
+  import Nav from '../components/Nav.svelte'
+  import Footer from '../components/Footer.svelte'
+  //export let segment;
 </script>
 
-<style>
-	main {
-		position: relative;
-		min-height: calc(100vh - 3em);
-		background-color: #fff;
-		overflow: hidden;
-	}
-	:global(main > section) {
-		max-width: 1650px;
-        margin: 6.5em auto 5em auto;
-		position: relative;
-	}
-	header {
-		position: fixed;
-		width: 100%;
-		background-color: #fff;
-		z-index: 999;
-		top: 0;
-		box-shadow: rgb(0 0 0 / 18%) 0px 1px 12px;
-    	border-bottom: rgb(0 0 0 / 18%);
-	}
-	footer {
-        width: 100%;
-        background-color: #efefef;
-        padding: .5em 0;
-    }
-
-</style>
-
-
 <header>
-	<Nav />
+  <Nav />
 </header>
 
-
 <main>
-	<slot></slot>
+  <slot />
 </main>
 <footer>
-	<Footer />
+  <Footer />
 </footer>
 
+<style>
+  main {
+    position: relative;
+    min-height: calc(100vh - 3em);
+    background-color: #fff;
+    overflow: hidden;
+  }
+  :global(main > section) {
+    max-width: 1650px;
+    margin: 6.5em auto 5em auto;
+    position: relative;
+  }
+  header {
+    position: fixed;
+    width: 100%;
+    background-color: #fff;
+    z-index: 999;
+    top: 0;
+    box-shadow: rgb(0 0 0 / 18%) 0px 1px 12px;
+    border-bottom: rgb(0 0 0 / 18%);
+  }
+  footer {
+    width: 100%;
+    background-color: #efefef;
+    padding: 1em 0;
+  }
+</style>
