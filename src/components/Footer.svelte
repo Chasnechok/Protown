1bb69d05f0510284703857f3c37b640d1efca97f
Artem Sukhorukov <artem@sukhorukov.me>
2024-10-21 11:04:04 +0200

    chore: remove link in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,14 +3,6 @@
 </script>
 
 <div class="footer-content">
-  <a href="https://www.country.ua/" target="_blank" class="country-ua-link"
-    ><img
-      src="https://www.country.ua/img/logo.svg"
-      height="25"
-      border="0"
-      alt="country.ua"
-    /></a
-  >
   <span>© {year} Все права защищены.</span>
 </div>
 
@@ -21,12 +13,5 @@
     align-items: center;
     gap: 8px;
     color: #7d7d7d;
-  }
-  .country-ua-link {
-    padding: 5px;
-    background-color: #fff;
-    border: 1px solid #ddd;
-    border-radius: 3px;
-    display: flex;
   }
 </style>
